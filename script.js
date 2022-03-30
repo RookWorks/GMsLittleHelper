066cf2ac3dfcfdbc5bc1810bede9dc01438860af
Cam <64113264+RookWorks@users.noreply.github.com>
2022-03-30 19:00:13 -0400

    getting closer to the first step of showing the card correctly

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const monsterCloseBtn = document.getElementById('monster-close-btn');
 
 //event listeners
 searchBtn.addEventListener('click', getMonsterList);
-monsterList.addEventListener('click',getMonsterStats);
+monsterList.addEventListener('click', getMonsterStats);
 monsterCloseBtn.addEventListener('click', () => {
     monsterDetailsContent.parentElement.classList.remove('showStats');
 });
@@ -13,20 +13,20 @@ monsterCloseBtn.addEventListener('click', () => {
 //get monster list that matches with tags
 function getMonsterList(){
     let searchInputTxt = document.getElementById('search-input').value.trim();
-    fetch(`https://www.dnd5eapi.co/api/monsters/${searchInputTxt}`)
+    fetch(`https://www.dnd5eapi.co/api/monsters?name=${searchInputTxt}`)
     .then(response => response.json())
     .then(data => {
          console.log(data);
             let html = "";
-            if(data.monsters){
-                data.monsters.forEach(monster => {
+            if(data.results){
+                data.results.forEach(results => {
                     html +=`
-                        <div class="monster-card" data-id = "${monster.index}>
+                        <div class="monster-card" data-id = "${results.index}>
                             <div class="monster-img">
                                  <img src = "http://placehold.jp/150x150.png" alt="monster">
                             </div>
                             <div class="monster-name">
-                                <h3>${monster.name}</h3>
+                                <h3>${results.name}</h3>
                                 <a href="#" class="monster-btn">Get Stats</a>
                             </div>
                         </div>
@@ -65,8 +65,8 @@ function monsterStatsModal(monster){
     //console.log(monster);
     monster = monster[0];
     let html = `
-            <h2 class = "monster-title">${monster.strMonster}</h2>
-            <p class="monster-type">${monster.strType}</p>
+            <h2 class = "monster-title">${monster.name}</h2>
+            <p class="monster-type">${monster.type}</p>
         <div class="monster-stats-img">
             <img src="http://placehold.jp/400x400.png" alt="">
         </div>
@@ -76,7 +76,7 @@ function monsterStatsModal(monster){
         </div>
 
         <div class="monster-link">
-            <a href="${monster.strDNDBeyond}" target="_blank">DnDBeyond Page</a>
+            <a href="https://www.dndbeyond.com/monsters" target="_blank">DnDBeyond Page</a>
         </div>
     `;
     monsterDetailsContent.innerHTML = html;
